206e50b6d03dd251427a4f9aaeedc3a7ce6bfd49
Marcos Magadán Rodríguez <Leapbs2017@gmail.com>
2025-09-24 11:51:56 +0200

    U1_Ejercicio02 Completo

--- Unidad_01/U1_Practica01/U1_Ejercicio02.php
+++ Unidad_01/U1_Practica01/U1_Ejercicio02.php
@@ -1,20 +1,21 @@
 <?php
 
-// Creamos las variables e inicializamos el mayor a 0
+// Creamos las variables
 $num1 = 2;
 $num2 = 10;
-$num3 = 7;
-$mayor = $numeros[0];
+$num3 = 8;
 
-// Creamos un array con los números
-$numeros = [$num1,$num2,$num3];
+
+// Creamos un array con los números e inicializamos el mayor a 0
+$numeros = array($num1,$num2,$num3);
+$mayor = $numeros[0];
 
 // Iteramos sobre el array para descubrir cual es mayor
 foreach($numeros as $num){
     if($num > $mayor){
         $mayor = $num;
     }
+}
 
 echo "El mayor es: " . $mayor;
-}
 ?>
