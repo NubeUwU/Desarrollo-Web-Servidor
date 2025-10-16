3e9f8c572a92f7d06c16365858f3ec75c260da13
Marcos Magadán Rodríguez <Leapbs2017@gmail.com>
2025-10-16 12:41:27 +0200

    Ejemplos U4

---
+++ Unidad_04_Formularios/Ejemplos/Ejemplo_Suma_Formulario_03.php
@@ -0,0 +1,35 @@
+<?php
+
+echo <<<_END
+<html>
+    <head>
+    <title>Form Test</title>
+    </head>
+<body>
+    <form method="post" action="Ejemplo_Suma_Formulario_03.php">
+        <label for="num1">Número 1:</label>
+        <input id="num1" name="num1">
+        <br><br>
+
+        <label for="num2">Número 2:</label>
+        <input id="num2" name="num2">
+        <br><br>
+        
+        <input type="submit" id="button" name="boton" value="Enviar">
+    </form>
+    </body>
+</html>
+_END;
+
+
+// Muestra el resultado al final
+if (isset($_POST['num1']) && isset($_POST['num2'])){
+    $num1 = intval($_POST['num1']);
+    $num2 = intval($_POST['num2']);
+    $resultado = $num1 + $num2;
+    
+    echo "<h1>El resultado es: $resultado</h1>";
+}
+
+
+?>
